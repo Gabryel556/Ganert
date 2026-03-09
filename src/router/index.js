1f3c288ff19d74d954147b94023949558fc1a10a
Ganert <gabryelnunes7483@gmail.com>
2026-03-08 23:37:09 -0300

    fix: ajuste de rota para dominio raiz e protecao contra erro 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Sites from '../views/Sites.vue';
 import Apps from '../views/Apps.vue';
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/Ganert/'), 
+  history: createWebHashHistory(), 
   routes,
 });
 
